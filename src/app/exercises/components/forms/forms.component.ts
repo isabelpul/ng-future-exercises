6f8f94858fceb1ab65f683c2ea1be5736caee725
Isabel Pérez <38857175+isabelpul@users.noreply.github.com>
2024-04-16 09:39:41 +0000

    discard forms changes

--- src/app/exercises/components/forms/forms.component.ts
+++ src/app/exercises/components/forms/forms.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core'
-import { ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-forms',
   standalone: true,
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css',
-  imports: [ReactiveFormsModule],
+  imports: [],
 })
 export class FormsComponent {}
--- src/app/exercises/components/hello-world/hello-world.component.ts
+++ src/app/exercises/components/hello-world/hello-world.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  template: ``,
+  template: `<p>hello world</p>`,
 })
 export class HelloWorldComponent {}
